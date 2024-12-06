317c8a1ed905354eb359f5643cfec51060d1f75a
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-12-06 14:12:27 +0000

    Add changes to backend

--- apps/backend/main.go
+++ apps/backend/main.go
@@ -3,53 +3,15 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/kelsiesmurphy/offline-collective-backend/database"
-
-	"github.com/resend/resend-go/v2"
 )
 
 type NewsletterContact struct {
 	FirstName string
 	LastName  string
 	Email     string
-}
-
-func createNewsletterContact(context *gin.Context) {
-	resendApiKey := os.Getenv("RESEND_API_KEY")
-
-	var body NewsletterContact
-	if err := context.BindJSON(&body); err != nil {
-		context.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "Bad Input", "details": err.Error()})
-		return
-	}
-
-	client := resend.NewClient(resendApiKey)
-	params := &resend.CreateContactRequest{
-		Email:        body.Email,
-		FirstName:    body.FirstName,
-		LastName:     body.LastName,
-		Unsubscribed: false,
-		AudienceId:   "a230d224-57a2-4afb-8e58-00bf651507fc",
-	}
-
-	contact, err := client.Contacts.Create(params)
-	if err != nil {
-		fmt.Println("Error creating NewsletterContact:", err)
-		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Failed to create contact"})
-		return
-	}
-
-	sqlStatement := "INSERT INTO newsletter_contact (id, email, first_name, last_name) VALUES ($1, $2, $3, $4);"
-	_, err = database.Db.Exec(sqlStatement, contact.Id, body.Email, body.FirstName, body.LastName)
-	if err != nil {
-		fmt.Println("SQL Error:", err)
-		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": "Couldn't create the new newsletter contact"})
-	} else {
-		context.JSON(http.StatusOK, gin.H{"message": "Newsletter Contact is successfully created."})
-	}
 }
 
 type Item struct {
@@ -82,13 +44,11 @@ func main() {
 
 	route.GET("/ping", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pang",
 		})
 	})
 
 	route.POST("/add", addItem)
-
-	route.POST("/newsletter-contact", createNewsletterContact)
 
 	err := route.Run(":8080")
 	if err != nil {
