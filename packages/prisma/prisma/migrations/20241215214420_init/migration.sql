346074b7dc1cffb39bc53fb9575ae4e9b8f2694f
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2024-12-17 21:42:20 +0000

    Move Prisma from package into /api

--- packages/prisma/prisma/migrations/20241215214420_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Survey" ALTER COLUMN "deletedAt" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "SurveyResponse" ALTER COLUMN "deletedAt" DROP NOT NULL;
--- packages/prisma/prisma/migrations/20241215215451_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "ExampleBusinessProduct" ADD COLUMN     "image" TEXT;
